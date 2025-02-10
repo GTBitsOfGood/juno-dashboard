1f22039f52a5a6e05dac05d8de9e7ae9530e64da
Yerti <36551149+UZ9@users.noreply.github.com>
2025-02-10 17:14:19 -0500

    add src better

---
+++ src-better/app/(dashboard)/projects/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+interface ProjectsLayoutProps {
+  children: ReactNode;
+}
+
+const ProjectsLayout = async ({ children }: ProjectsLayoutProps) => {
+  return <>{children}</>;
+};
+
+export default ProjectsLayout;

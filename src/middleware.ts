30dee3b056d01afc67be0cd93258950d4d0ea89c
root <root@LAPTOP-RVBT0RUV>
2025-03-28 16:18:50 -0400

    Removed any usage

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { decodeJwt } from "jose";
 
+interface JWTPayload {
+  user: {
+    type: number;
+  };
+}
+
 export function middleware(req: NextRequest) {
   try {
     const token = req.cookies.get("jwt-token");
-    const value = decodeJwt(token.value) as any;
+    const value: JWTPayload = decodeJwt(token.value);
     if (token && value.user.type != 2) {
       return NextResponse.next();
     } else {
       return NextResponse.redirect(new URL("/", req.url));
     }
-  } catch (Exception) {
+  } catch {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 }

8734b52d3875da08ab8723986a4ce1a5b8f38f16
Long Lam <llam36@gatech.edu>
2025-10-18 13:13:34 -0400

    feat: add checkboxes to tables

--- src/components/emailConfigTable/columns.tsx
+++ src/components/emailConfigTable/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Checkbox } from "@/components/ui/checkbox";
 import { ColumnDef } from "@tanstack/react-table";
 
 export type EmailConfig = {
@@ -12,12 +13,34 @@ export type EmailConfig = {
 
 export const columns: ColumnDef<EmailConfig>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        className="ms-2 align-middle mr-5"
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        className="ms-2 align-middle"
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+      />
+    ),
+    size: 50,
+  },
+  {
     accessorKey: "id",
-    header: "Config ID",
+    header: "ID",
   },
   {
     accessorKey: "environment",
     header: "Environment",
+  },
+  {
+    accessorKey: "sendgridKey",
+    header: "Sendgrid Key",
   },
   {
     accessorKey: "domainNames",
--- src/components/fileConfigTable/columns.tsx
+++ src/components/fileConfigTable/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Checkbox } from "@/components/ui/checkbox";
 import { ColumnDef } from "@tanstack/react-table";
 
 export type FileConfig = {
@@ -10,8 +11,26 @@ export type FileConfig = {
 
 export const columns: ColumnDef<FileConfig>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        className="ms-2 align-middle mr-5"
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        className="ms-2 align-middle"
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+      />
+    ),
+    size: 50,
+  },
+  {
     accessorKey: "id",
-    header: "Config ID",
+    header: "ID",
   },
   {
     accessorKey: "environment",
@@ -20,5 +39,9 @@ export const columns: ColumnDef<FileConfig>[] = [
   {
     accessorKey: "bucketNames",
     header: "Buckets",
+  },
+  {
+    accessorKey: "fileNames",
+    header: "Files",
   },
 ];

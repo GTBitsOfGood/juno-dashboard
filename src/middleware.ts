9944c7216dc3541e3f9efe4b777ac3000996f193
David Gu <dawsomegu@gmail.com>
2025-10-25 16:34:54 -0400

    Readded logic in middleware.ts to have non admins be redirected to dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,10 +17,14 @@ export function middleware(req: NextRequest) {
   try {
     const token = req.cookies.get("jwt-token");
     const value: JWTPayload = decodeJwt(token.value);
-    if (token && value.user.type != 2) {
-      return NextResponse.next();
+    if (pathname.startsWith("/admin")) {
+      if (token && value.user.type != 2) {
+        return NextResponse.next();
+      } else {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
     } else {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
+      return NextResponse.next();
     }
   } catch {
     return NextResponse.redirect(new URL("/login", req.url));

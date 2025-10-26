cb7ca9c99b844ae6114cb52e776c94987b0f1f08
Long Lam <llam36@gatech.edu>
2025-10-26 01:30:18 -0400

    feat: add analytic config table

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -30,10 +30,10 @@ export async function getAnalyticsConfig(
 ): Promise<AnalyticsConfigResponse> {
   const junoClient = getJunoInstance();
   try {
-  const analyticsConfig =
-    await junoClient.analyticsConfig.getAnalyticsConfig(projectId);
-  return JSON.parse(JSON.stringify(analyticsConfig));
-} catch (e) {
+    const analyticsConfig =
+      await junoClient.analyticsConfig.getAnalyticsConfig(projectId);
+    return JSON.parse(JSON.stringify(analyticsConfig));
+  } catch (e) {
     if (e?.body.toLowerCase().includes("not_found")) {
       throw new Error("Analytics Config does not exist");
     }

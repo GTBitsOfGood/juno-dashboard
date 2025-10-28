3428cac6d3fc32f07d63e49bad63c42c7b1dfc27
David Gu <dawsomegu@gmail.com>
2025-10-28 16:42:09 -0400

    Reverted middleware.ts to main

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,34 +9,19 @@ interface JWTPayload {
 }
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (publicRoutes.includes(pathname)) {
-    return NextResponse.next();
-  }
-
   try {
     const token = req.cookies.get("jwt-token");
-    if (!token) {
-      throw new Error("No token found");
-    }
     const value: JWTPayload = decodeJwt(token.value);
-    if (pathname.startsWith("/admin")) {
-      if (token && value.user.type != 2) {
-        return NextResponse.next();
-      } else {
-        return NextResponse.redirect(new URL("/dashboard", req.url));
-      }
-    } else {
+    if (token && value.user.type != 2) {
       return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
   } catch {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 
-const publicRoutes = ["/", "/login"];
-
 export const config = {
-  // matcher: ["/admin/:path*", "/projects/:path*", "/settings/:path*"],
-  matcher: ["/((?!api|_next|static|[.].*|favicon.ico).*)"], // Matches ALL routes besides API, _next, and static
+  matcher: ["/", "/admin/:path*"],
 };

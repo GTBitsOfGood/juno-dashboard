65ed7ce6de091374da9d4490d583e53feebca616
Keshav Worathur <keshavworathur@gmail.com>
2026-02-19 13:39:32 -0500

    feat(api-keys): implement three-card grid layout on api keys page

--- src/app/(dashboard)/admin/keys/page.tsx
+++ src/app/(dashboard)/admin/keys/page.tsx
@@ -91,7 +91,7 @@ export default function KeyPage() {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="container mx-auto px-10 py-10 flex flex-col gap-[18px]">
       <Breadcrumb className="mb-4">
         <BreadcrumbList>
           <BreadcrumbItem>
